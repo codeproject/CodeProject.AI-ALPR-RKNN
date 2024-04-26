ebcc1d5e03909f7c347c15b13713dc068b5983d6
Chris Maunder <chris.maunder@codeproject.com>
2024-04-26 16:16:48 -0400

    Correcting path to setup script

--- install.sh
+++ install.sh
@@ -4,12 +4,12 @@
 #
 # This script is called from the ALPR-RKNN directory using: 
 #
-#    bash ../../setup.sh
+#    bash ../../CodeProject.AI-Server/src/setup.sh
 #
 # The setup.sh file will find this install.sh file and execute it.
 
 if [ "$1" != "install" ]; then
-    read -t 3 -p "This script is only called from: bash ../../setup.sh"
+    read -t 3 -p "This script is only called from: bash ../../CodeProject.AI-Server/src/setup.sh"
     echo
     exit 1 
 fi
